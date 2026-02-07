6cb8a830ab7d4227e49d8c1c24a79307414e95c8
Jones8683 <outerrim1977@gmail.com>
2026-02-07 20:46:28 +1030

    feat: update header component

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
-const nav = document.querySelector("#navbar");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 20) {
-    nav.classList.add("minimized");
-  } else {
-    nav.classList.remove("minimized");
+class HeaderComponent extends HTMLElement {
+  constructor() {
+    super();
   }
-});
+
+  connectedCallback() {
+    let lastScrollY = window.scrollY;
+
+    this.classList.toggle("minimized", lastScrollY > 20);
+
+    window.addEventListener("scroll", () => {
+      const currentScrollY = window.scrollY;
+
+      this.classList.toggle("minimized", currentScrollY > 20);
+
+      this.dataset.show = (
+        currentScrollY < 350 || currentScrollY < lastScrollY
+      ).toString();
+
+      lastScrollY = currentScrollY;
+    });
+  }
+}
+
+customElements.define("header-component", HeaderComponent);
